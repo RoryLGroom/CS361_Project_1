de5df327d2224dc8c08810d278a63b0c9f7e98ca
RoryLGroom <groomro@oregonstate.edu>
2025-07-16 05:02:38 +0000

    Jul 15, 2025, 10:02 PM

--- functions.cpp
+++ functions.cpp
@@ -2,3 +2,16 @@
 using namespace std;
 #include "profile.h"
 
+bool newUserValidator(const Users& u, string email)
+{
+  for (int i = 0; i < u.existingUsers.size(); i++) {
+    if (u.existingUsers[i].email == email) {
+      return false;
+    }
+  }
+  return true;
+}
+
+profile newUser(string& name, string& DOB, string& email, string& password)
+{
+}
