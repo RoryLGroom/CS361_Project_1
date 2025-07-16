ba8c8b9dcfe8bb587da1898eb66b78c9fd9498fb
RoryLGroom <groomro@oregonstate.edu>
2025-07-15 18:37:01 -0700

    committing full folder

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "profile.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string yesOrNo;
+  cout << "Welcome! Are you an existing user?" << endl;
+  cout << "Enter YES to be directed to login screen." << endl;
+  cout << "enter NO to create new profile" << endl;
+  cin >> yesOrNo;
+}
